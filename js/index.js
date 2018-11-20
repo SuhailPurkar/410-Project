d9822cff692de181247bc12917a9f8390d5fe43e
Suhail Purkar <suhailpurkar@gmail.com>
2018-11-20 13:21:23 -0500

    Added content to windows

--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
 /*var mainText = document.getElementById("mainText");*/
 var submitBtn = document.getElementById("submitBtn");
 
-function submitClick(value) {
+function submitClick_1(value) {
 	var firebaseRef = firebase.database().ref();
 
 	/*var messageText = mainText.value;*/
 
 	firebaseRef.child("Project 1").child("Suhail").child("Prefrence").set(value);
+
+}
+
+function submitClick_2(value) {
+	var firebaseRef = firebase.database().ref();
+
+	/*var messageText = mainText.value;*/
+
+	firebaseRef.child("Project 2").child("Suhail").child("Prefrence").set(value);
+
+}
+
+function submitClick_3(value) {
+	var firebaseRef = firebase.database().ref();
+
+	/*var messageText = mainText.value;*/
+
+	firebaseRef.child("Project 3").child("Suhail").child("Prefrence").set(value);
 
 }
