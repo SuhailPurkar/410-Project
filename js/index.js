c0bfb755a0713cd657a21caf319973e3d6ea099b
Suhail Purkar <suhailpurkar@gmail.com>
2018-11-20 12:48:21 -0500

    Adding prefrence buttons to smaller windows

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 /*var mainText = document.getElementById("mainText");*/
 var submitBtn = document.getElementById("submitBtn");
 
-function submitClick(p1) {
+function submitClick(value) {
 	var firebaseRef = firebase.database().ref();
 
 	/*var messageText = mainText.value;*/
 
-	firebaseRef.child("Project 1").child("Suhail").child("Prefrence").set(p1);
+	firebaseRef.child("Project 1").child("Suhail").child("Prefrence").set(value);
 
 }
