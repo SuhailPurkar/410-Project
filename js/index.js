04563f38ec1257ddb7e706c0fa6ec1cf8f1c1a97
Suhail Purkar <suhailpurkar@gmail.com>
2018-11-22 07:50:35 -0500

    Added authentication with connection to database

--- js/index.js
+++ js/index.js
@@ -1,19 +1,61 @@
-/*var mainText = document.getElementById("mainText");*/
+/* firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    $(".login-cover").hide();
+
+    var dialog = document.querySelector('#loginDialog');
+    if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.close();
+  } else {
+    var dialog = document.querySelector('#loginDialog');
+    if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.showModal();
+  }
+});*/
+
+/*$("#loginBtn").click(
+	function(){
+		var email = $("#loginEmail").val();
+		var password = $("#loginPassword").val();
+
+		
+
+			$("#checkingEmail").text(email);
+			$("#checkingPassword").text(password);
+			
+
+			firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+			 
+			  $("#loginError").show().text(error.message);
+			 
+			});
+		
+	}
+
+);
+*/
+
+
+
+/*
 var submitBtn = document.getElementById("submitBtn");
 
 function submitClick_1(value) {
 	var firebaseRef = firebase.database().ref();
 
-	/*var messageText = mainText.value;*/
+	
 
-	firebaseRef.child("Project 1").child("Suhail").child("Prefrence").set(value);
+	firebaseRef.child().child("Project 1").child("Prefrence").set(value);
 
 }
 
 function submitClick_2(value) {
 	var firebaseRef = firebase.database().ref();
 
-	/*var messageText = mainText.value;*/
+	
 
 	firebaseRef.child("Project 2").child("Suhail").child("Prefrence").set(value);
 
@@ -22,8 +64,9 @@ function submitClick_2(value) {
 function submitClick_3(value) {
 	var firebaseRef = firebase.database().ref();
 
-	/*var messageText = mainText.value;*/
+	
 
 	firebaseRef.child("Project 3").child("Suhail").child("Prefrence").set(value);
 
-}
+}*/
+
